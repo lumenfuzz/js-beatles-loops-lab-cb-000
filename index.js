131df65d7579c89013a80155ce78b6faacb5a614
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 15:38:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
-
+  var newArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    newArray[i] = `${musicians[i]} plays ${instruments}`
+  }
+  return newArray
 }
 
 function johnLennonFacts(array) {
